9ddcd532de48bdf06e603790b89f58d9856244ee
Nora <nora.lyngstad.roeste@sparebank1.no>
2025-01-09 15:46:23 +0100

    Fargestatus og litt styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ export default function App() {
   const {data: sensor_2} = useMoisture(2)
   const {data} = useRecentMoisture()
 
+  const calculateColor = (moisture: number) => {
+    if (moisture < 10) {
+      return 'red'
+    } else if (moisture < 50) {
+      return 'yellow'
+    } else {
+      return 'green'
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,19 +26,26 @@ export default function App() {
         </h2>
       <div>
         {data?.map((moisture, index) => (
-          <div key={index}>
+          <div className='recent-moisture' key={index} style={{}}>
+
+            <div  className='recent-moisture__sensor-status' style={{
+              backgroundColor: calculateColor(moisture.moisturePercentage)
+            }}></div>
             <p>
-              Sensor {moisture.sensorId} has a moisture level of {moisture.moisturePercentage}%, updated at {moisture.updatedAt}
+
+              Sensor {moisture.sensorId} has a moisture level of {moisture.moisturePercentage}%, updated
+              at {moisture.updatedAt}
             </p>
+
           </div>
         ))}
       </div>
 
-      <table>
+      <table className='moisture-table'>
         <thead>
-          <tr>
-            <th colSpan={3}>
-              Recent moisture measurements
+        <tr>
+          <th colSpan={3}>
+            Recent moisture measurements
             </th>
           </tr>
         </thead>
