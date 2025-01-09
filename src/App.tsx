bc8b53dce17cd0421ed2a47b6844bef9fe40fcc2
Nora <nora.lyngstad.roeste@sparebank1.no>
2025-01-09 12:35:45 +0100

    Komponent pr sensor, sortert etter dato og maks 10 verdier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import {useMoisture} from "./useMoisture";
+import {SensorMoisture, useMoisture} from "./useMoisture";
 
 export default function App() {
 
-  const { data } = useMoisture(1)
+  const {data: sensor_1} = useMoisture(1)
+  const {data: sensor_2} = useMoisture(2)
 
   return (
     <div className="App">
@@ -11,26 +12,46 @@ export default function App() {
         Velkommen til fuktighetsmåleren
       </header>
       <table>
+        <tbody>
         <tr>
-        <th>
-          Sensor
-        </th>
-        <th>
-          Moisture percentage
-        </th>
-        <th>
-          Updated at
-        </th>
+          <th>
+            Sensor
+          </th>
+          <th>
+            Moisture percentage
+          </th>
+          <th>
+            Updated at
+          </th>
         </tr>
-      {data?.map((moisture) => (
+            <SensorValues sensorValues={sensor_1}/>
         <tr>
-          <td>{moisture.sensorId}</td>
-          <td>{moisture.moisturePercentage}</td>
-          <td>{moisture.updatedAt}</td>
-</tr>
-          ))}
+          <td>--</td>
+          <td>--</td>
+          <td>--</td>
+        </tr>
+            <SensorValues sensorValues={sensor_2}/>
+        </tbody>
       </table>
     </div>
   );
 }
 
+ function SensorValues({sensorValues}: { sensorValues: SensorMoisture[] | undefined }) {
+  const sortedAndSliced  = sensorValues?.sort((a: SensorMoisture, b: SensorMoisture) => {
+    return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+  })
+    .slice(0, 10)
+
+  return (
+    <>
+    {sortedAndSliced?.map((moisture) => (
+        <tr>
+          <td>{moisture.sensorId}</td>
+          <td>{moisture.moisturePercentage}</td>
+          <td>{moisture.updatedAt}</td>
+        </tr>
+      ))}
+    </>
+  )
+ }
--- src/useMoisture.ts
+++ src/useMoisture.ts
@@ -3,9 +3,11 @@ import {useQuery, UseQueryResult} from "@tanstack/react-query";
 const apiUrl = "https://moisture-surveillance-production.up.railway.app"
 export function useMoisture(sensor: number): UseQueryResult<SensorMoisture[]>   {
 
+  console.log("sensor", sensor)
   const MOISTURE_URL = `${apiUrl}/moisture?sensorId=${sensor}`;
+  console.log(MOISTURE_URL)
   return useQuery({
-    queryKey: ['moisture'],
+    queryKey: ['moisture', sensor],
     queryFn: async() => {
       const response = await fetch(MOISTURE_URL, {
           headers: {
